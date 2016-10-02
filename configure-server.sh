6b3cd1d01f44f73fa1834464430f0a281b1b6735
sahilgupta5 <sahilgupta5@hotmail.com>
2016-10-02 11:25:29 -0400

    Fixing https://github.com/aws/aws-cli/issues/366

--- configure-server.sh
+++ configure-server.sh
@@ -2,9 +2,10 @@
 
 sleep 60
 mkdir -p /usr/local/jenkins
+cd /usr/local/jenkins
 
 #Install git on the server
-sudo apt-get -y install git
+sudo apt-get --yes install git
 
 #Clone the repository to setup Jenkins server
 git clone https://github.com/sahilgupta5/ManageJenkins.git
