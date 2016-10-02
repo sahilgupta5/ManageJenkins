334ca6fd7ea034643afbd3f17f91a2054de69e5d
sahilgupta5 <sahilgupta5@hotmail.com>
2016-10-02 11:09:34 -0400

    Corrected the sequence of updates

--- configure-server.sh
+++ configure-server.sh
@@ -1,15 +1,14 @@
 # !/bin/bash
 
 sleep 60
-
-sudo ./updateServer.sh
+mkdir -p /usr/local/jenkins
 
 #Install git on the server
 sudo apt-get -y install git
 
 #Clone the repository to setup Jenkins server
 git clone https://github.com/sahilgupta5/ManageJenkins.git
-
 cd ManageJenkins
 
+sudo ./updateServer.sh
 sudo ./installJenkinsAndApache.sh
