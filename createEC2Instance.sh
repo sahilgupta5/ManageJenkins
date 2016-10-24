2fbe376f825aea369abb785842360a351f3c9539
sahilgupta5 <sahilgupta5@hotmail.com>
2016-10-24 02:41:17 -0400

    Creation of jenkins server indepedant of path.

--- createEC2Instance.sh
+++ createEC2Instance.sh
@@ -16,7 +16,7 @@ SG_NAME=$(./createSGGroup.sh)
 echo "Created security group: $SG_NAME"
 
 #Create an AWS EC2 instance using Ubuntu 14.04 LTS image
-EC2_INSTANCE_ID=$(aws ec2 run-instances --image-id ami-d732f0b7 --user-data file:///Users/sahilgupta/Google\ Drive/Coding/Eclipse\ Workplace/ManageJenkins/configure-server.sh --count 1 --instance-type t2.small --key-name $KEY_PAIR_NAME --security-groups $SG_NAME | jq .Instances[0].InstanceId | tr -d '"')
+EC2_INSTANCE_ID=$(aws ec2 run-instances --image-id ami-d732f0b7 --user-data file://configure-server.sh --count 1 --instance-type t2.small --key-name $KEY_PAIR_NAME --security-groups $SG_NAME | jq .Instances[0].InstanceId | tr -d '"')
 
 echo "Starting an EC2 instance for the jenkins server and installing jenkins on it: $EC2_INSTANCE_ID"
 aws ec2 describe-instances --instance-ids $EC2_INSTANCE_ID > $DIR_NAME/$EC2_INSTANCE_ID.json
