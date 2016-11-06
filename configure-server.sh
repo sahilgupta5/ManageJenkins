dc7ed94934bcf3fbc447db02c7dbf4121be443b7
sahilgupta5 <sahilgupta5@hotmail.com>
2016-11-06 15:23:35 -0500

    Updating the permissions

--- configure-server.sh
+++ configure-server.sh
@@ -8,7 +8,7 @@ sudo apt-get --yes install git
 #Clone the repository to enable launching CARMAISE
 git clone https://github.com/sahilgupta5/CARMAISE.git
 chown -R jenkins:jenkins /usr/local/jenkins/CARMAISE
-chmod -R 755 /usr/local/jenkins/CARMAISE
+chmod -R 777 /usr/local/jenkins/CARMAISE
 
 #Clone the repository to setup Jenkins server
 git clone https://github.com/sahilgupta5/ManageJenkins.git
